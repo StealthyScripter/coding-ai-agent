9f40bda928bbdcfc9a4554a2f660943a74fc4e4e
StealthyScripter <wendotbrian@gmail.com>
2025-05-01 22:14:58 -0400

    added the main logic using the fetch function and passing it through the chain

---
+++ main.py
@@ -0,0 +1,70 @@
+from dotenv import load_dotenv
+import os
+
+from langchain_openai import ChatOpenAI, OpenAIEmbeddings
+from langchain_astradb import AstraDBVectorStore
+from langchain.agents import create_tool_calling_agent, AgentExecutor
+from langchain.tools.retriever import create_retriever_tool
+from langchain import hub
+from github import fetch_github_issues
+from note import note_tool
+
+load_dotenv()
+
+def connect_to_vstore():
+    embeddings = OpenAIEmbeddings()
+    ASTRA_DB_API_ENDPOINT = os.getenv("ASTRA_DP_API_ENDPOINT")
+    ASTRA_DB_APPLICATION_TOKEN = os.getenv("ASTRA_DB_APPLICATION_TOKEN")
+    desired_namespace = os.getenv("ASTRA_DB_KEYSPACE")
+
+    if desired_namespace:
+        ASTRA_DB_KEYSPACE = desired_namespace
+    else:
+        ASTRA_DB_KEYSPACE = None
+    
+    vstore = AstraDBVectorStore(
+        embedding=embeddings,
+        collection_name="wibuddy-agent",
+        api_endpoint=ASTRA_DB_API_ENDPOINT,
+        token=ASTRA_DB_APPLICATION_TOKEN,
+        namespace=ASTRA_DB_KEYSPACE
+    )
+    return vstore
+
+vstore = connect_to_vstore()
+add_to_vectorstore = input("Do you want to update issues? (y/N): ").lower() in ["yes", "y",]
+
+if add_to_vectorstore:
+    owner="techwithtim"
+    repo="Flask-Web-App-Tutorial"
+    issues = fetch_github_issues(owner, repo)
+
+    try:
+        vstore.delete_collection()
+    except:
+        pass
+
+    vstore = connect_to_vstore()
+    vstore.add_documents(issues)
+
+    # results = vstore.similarity_search("flash messages", k=3)
+    # for res in results:
+    #     print(f"* {res.page_content} {res.metadata}")
+
+retriever = vstore.as_retriever(search_kwargs={"k":3})
+retriever_tool = create_retriever_tool(
+    retriever,
+    "github_search",
+    "Descriptions"
+)
+
+prompt = hub.pull("hwchase17/openai-functions-agent")
+llm = ChatOpenAI()
+
+tools = [retriever_tool, note_tool]
+agent = create_tool_calling_agent(llm, tools, prompt)
+agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True)
+
+while (question := input("Ask a question about github issues (q to quit): ")) != "q":
+    result = agent_executor.invoke({"input": question})
+    print(result["output"])
